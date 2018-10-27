1084cbbb86c8bf9e52551a2cc43f9564fcbaeb3a
Roberto Samia <bojosteph@gmail.com>
2018-10-27 23:53:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,7 +10,8 @@ def sort_array_char_count(array)
   array.sort{ |a, b| a.length <=> b.length}
 end
 
-def swap_elements_from_to(array, index, destination_index)
+def swap_elements_from_to(array)
+  array
      
   end
   
