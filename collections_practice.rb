1b7a1fc7ba62a9fcde7fff573868352802315874
Roberto Samia <bojosteph@gmail.com>
2018-10-28 01:47:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,14 @@ def find_a(array)
  end
  
  def add_s(array)
-   array.each_with_index.collect{|element, index| }
+   
+   array.each_with_index.collect{|element, index|}
+   if element != array[1]
+     element.each{ |x| x << "s"}
+      
+    end
+  end
+    
    
    
  
