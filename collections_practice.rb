0f7cc6a78002f502478ed299d283a11633f2b0fa
Roberto Samia <bojosteph@gmail.com>
2018-10-28 00:38:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,10 +22,10 @@ def reverse_array(array)
 end 
 
 def kesha_maker(array)
-   new_array = []
+   
    array.each do |element|
-    new_array = element.insert(index[2], "$")
-    new_array
+  element.insert(index[2], "$")
+    
   end
 end
   
