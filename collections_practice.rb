eeefe197a9c784a04afcfcd1e6c284e359cbcb8e
Roberto Samia <bojosteph@gmail.com>
2018-10-28 00:21:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,7 +24,7 @@ end
 def kesha_maker(array)
    new_array = []
   array.each do |element|
-   new_array = element.split << element[2] << "$"
+   new_array = element[2] << "$"
    new_array
  end
  end
